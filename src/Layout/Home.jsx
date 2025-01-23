a0b2eefd3a12f4d4ce495bc003b0b2ee09e25dd5
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-01-24 00:08:01 +0700

    UI

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -8,7 +8,7 @@ function Home(props) {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7" data-aos="fade-up-right">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-                    Design with Friendly UX/Ui  
+                    Design with Friendly UX/UI 
                     </h1>
                     <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                     Start developing with an open-source library UI components, sections, and pages built with the utility classes.
