f93893496b2183232063676a3466025866bd20ea
engsreynich <esreynich6@gmail.com>
2025-01-23 08:50:21 +0700

    Done AboutUS

--- src/Layout/Docs.jsx
+++ src/Layout/Docs.jsx
@@ -79,7 +79,8 @@ function Docs() {
                         <pre className="bg-pink-100 dark:bg-pink-900 text-pink-600 dark:text-pink-200 p-4 rounded-lg mt-4 overflow-auto mt-3 flex items-start">
                             <i className="fa-regular fa-copy text-white"></i>
     
-                            <code>&lt;script src="https://cdn.tailwindcss.com"&gt;&lt;/script&gt;</code>
+                            {/* <code>&lt;script src="https://cdn.tailwindcss.com"&gt;&lt;/script&gt;</code> */}
+                            <code>&lt;https://rachna-cdn.cloudinator.cloud/css/tailwind.css &lt;</code>
                         </pre>
                     </section>
 
